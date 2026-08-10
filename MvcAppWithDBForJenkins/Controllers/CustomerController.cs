99d2ba88015e9e91e297d840f2cea861c5f075a6
ciitinstitutepune <ciitpune@gmail.com>
2026-08-10 20:50:06 +0530

    dfgf

---
+++ MvcAppWithDBForJenkins/Controllers/CustomerController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcAppWithDBForJenkins.Models;
+
+namespace MvcAppWithDBForJenkins.Controllers
+{
+    public class CustomerController : Controller
+    {
+        CoreapidbContext db;
+        public CustomerController(CoreapidbContext db)
+        {
+            this.db = db;
+        }
+        public IActionResult Index()
+        {
+            ViewData["customers"] = db.TblCustomers.ToList();
+            return View();
+        }
+    }
+}
